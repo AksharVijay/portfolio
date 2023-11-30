aa4d301b7398807dd23b08eb02117d935b463a96
Akshar Vijay <aksharvij@gmail.com>
2023-12-01 03:21:14 +1300

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from "./Header.module.css";
+import { Link } from "react-scroll";
+import contact from  "../../assets/contact.png"
 
 const Header = () => {
   return (
     <>
       <nav className={styles.navbar}>
-        <img src="" alt="" className={styles.logo} />
+        <div className={styles.logo}>Akshar Vijay</div>
         <div className={styles.menu}>
-          
+          <Link to="/" className={styles.menuListItem}>Home</Link>
+          <Link to="/About"className={styles.menuListItem}>About</Link>
+          <Link to="/Portfolio"className={styles.menuListItem}>Portfolio</Link>
         </div>
         <button className={styles.contactHeader}>
-          <img src="" alt="" className={styles.contactIcon} />
+          <img src={contact} alt="" className={styles.contactIcon} />
           Contact
         </button>
       </nav>
